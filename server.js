e95fc4f04399c16e18028568690c859cb5d74004
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-09-12 14:14:06 -0400

    fixed start script

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const projectRouter = require('./routers/projectRouter');
 const actionRouter = require('./routers/actionRouter');
 const cors = require('cors');
 const server = express();
+
 server.use(cors());
 server.use(logger());
 server.use(express.json());
+
 server.use('/api/projects', projectRouter);
 server.use('/api/actions', actionRouter);
+
 server.use((err, req, res, next) => {
 	console.log(err);
 	res.status(500).json({ message: 'Something went wrong, try again...' });
